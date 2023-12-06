0c59d3d2cf10ba8a4143475178c4430dd98cb035
Bart Huiskamp <bartolameus.h@gmail.com>
2023-12-06 14:32:08 +0100

    added save and cancel butten when notes are clicked

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ const App = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
-  const [SelectedNote, setSelectedNote] =
+  const [selectedNote, setSelectedNote] =
   useState<Note | null>(null);
 
   const handleNoteClick = (note:Note) => {
@@ -65,6 +65,40 @@ const App = () => {
       setContent("");
     };
     
+    const handleUpdateNote = (
+      event: React.FormEvent
+    ) => {
+      event.preventDefault();
+
+      if(!selectedNote){
+        return;
+      }
+
+      const updatedNote: Note = {
+        id: selectedNote.id,
+        title: title,
+        content: content,
+      }
+
+      const updatedNotesList = notes.map((note)=>
+      note.id === selectedNote.id
+        ? updatedNote
+        : note
+      )
+
+      setNotes(updatedNotesList);
+      setTitle("")
+      setContent("")
+      setSelectedNote(null);
+    };
+
+    const handleCancel = () => {
+      setTitle("")
+      setContent("")
+      setSelectedNote(null);
+    }
+  
+
   return(
   <div className="app-container">
     <form className="note-form" 
@@ -86,9 +120,17 @@ const App = () => {
       rows={10}
       required
       ></textarea>
-      <button  type="submit" >
-        Add Note
-      </button>
+
+      {selectedNote ? (
+        <div className="edit-buttons">
+          <button type="submit">Save</button>
+          <button onClick={handleCancel}>
+            Cancel
+          </button>
+        </div>
+      ) : (
+        <button type="submit">Add Note</button>
+      )}
     </form>
       <div className="notes-grid">
         {notes.map((note)=> (
